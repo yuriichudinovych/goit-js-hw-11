0b32b1e3f7f73bc6cd5a9392c5fbbdfd18c18f94
Yurii Chudinovych <yuriichudinovych93@gmail.com>
2022-08-13 16:46:39 +0300

    improve code

---
+++ src/js/fetch-service.js
@@ -0,0 +1,59 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default class FetchService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.totalHits = 0;
+  }
+  fetchData() {
+    const options = new URLSearchParams({
+      key: '29162524-01f0dd46893302e996c3171e6',
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page: this.page,
+    });
+    return fetch(`${BASE_URL}?${options}`)
+      .then(r => {
+        if (!r.ok) {
+          throw new Error(r.status);
+        }
+        return r.json();
+      })
+      .then(data => {
+        this.incrementPage();
+        if (data.hits.length === 0) {
+          return Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        }
+        this.totalHits = data.totalHits;
+        return data.hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  totalHitsMessage() {
+    return Notify.success(`Hooray! We found ${this.totalHits} images.`);
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,94 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import fetchImg from './fetchImg.js';
+import FetchService from './fetch-service.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchFormRef = document.querySelector('#search-form');
 const gelleryRef = document.querySelector('.gallery');
+const loadMoreBtnRef = document.querySelector('.load-more');
 
+const fetchService = new FetchService();
+console.log(fetchService.totalHits);
 
+searchFormRef.addEventListener('submit', onSearch);
+loadMoreBtnRef.addEventListener('click', onLoadMore);
 
-searchFormRef.addEventListener('submit', e => {
-  e.preventDefault();
-  const {
-    elements: { searchQuery },
-  } = e.currentTarget;
-  const category = searchQuery.value;
-
-  if (category.trim() === "" ) {
-    return Notify.failure("Please fill the field!");
+function onSearch(evt) {
+  evt.preventDefault();
+  clearGalleryList();
+  fetchService.query = evt.currentTarget.elements.searchQuery.value;
+  fetchService.resetPage();
+  if (fetchService.query.trim() === '') {
+    return Notify.failure('Please fill the field!');
   }
 
- 
-
-  fetchImg(category).then(({hits}) => {
-    console.log(hits)
-
-    if(hits.length === 0) {
-      return Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-    }
-    createImgList(hits)}).catch(error => {
-console.log(error)
-      
+  fetchService
+    .fetchData()
+    .then(hits => {
+      createImgList(hits);
+      fetchService.totalHitsMessage();
+      lightbox();
+    })
+    .catch(error => {
+      console.log(error);
     });
-});
+}
 
-
+function onLoadMore() {
+  fetchService
+    .fetchData()
+    .then(hits => {
+      createImgList(hits);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
 
 function createImgList(cards) {
-console.log(cards.length)
   const markup = cards.map(card => {
     return `<div class="photo-card">
+    <a href='${card.largeImageURL}' >
     <img src="${card.webformatURL}" alt="${card.tags}" loading="lazy" />
+    </a>
     <div class="info">
       <p class="info-item">
-        <b>Likes ${card.likes}</b>
+        <b>Likes</b>
+        ${card.likes}
       </p>
       <p class="info-item">
-        <b>Views</b>
+        <b>Views </b>
+        ${card.views}
       </p>
       <p class="info-item">
-        <b>Comments</b>
+        <b>Comments </b>
+        ${card.comments}
       </p>
       <p class="info-item">
-        <b>Downloads</b>
+        <b>Downloads </b>
+        ${card.downloads}
       </p>
     </div>
-  </div>`
-  })
- gelleryRef.innerHTML = markup.join('');
+  </div>
+  `;
+  });
+  gelleryRef.insertAdjacentHTML('beforeend', markup.join(''));
+}
+refreshlightbox(lightbox());
+
+function clearGalleryList() {
+  gelleryRef.innerHTML = '';
+}
+function lightbox() {
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+  });
+  return lightbox;
+}
+
+async function refreshlightbox(lightbox) {
+  console.log(lightbox);
+  lightbox.refresh();
 }
